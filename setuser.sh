b5ee84bff6dee9a55d4ae88102ba77c7b178d3fc
oak680 <131433825+oak680@users.noreply.github.com>
2023-05-23 19:57:48 +0700

    setuser.sh

--- setuser.sh
+++ setuser.sh
@@ -7,6 +7,6 @@ sudo dscl . -create /Users/vncuser RealName "User"
 sudo dscl . -create /Users/vncuser UniqueID 1001
 sudo dscl . -create /Users/vncuser PrimaryGroupID 80
 sudo dscl . -create /Users/vncuser NFSHomeDirectory /Users/vncuser
-sudo dscl . -passwd /Users/vncuser password
-sudo dscl . -passwd /Users/vncuser password
+sudo dscl . -passwd /Users/vncuser 254322
+sudo dscl . -passwd /Users/vncuser 254322
 sudo createhomedir -c -u vncuser > /dev/null
